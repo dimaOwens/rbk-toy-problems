5710127c136a372f6f6ed45960ae7345771e7bdc
dimaOwens <dimaOwens>
2020-09-09 09:43:48 +0300

    solved

--- W4D5.js
+++ W4D5.js
@@ -18,4 +18,54 @@ Would return:
 ["Ben Bitdiddle: August 19, 1984", "Lem Tweakit: August 11, 1989"]
 
 */
+function each(coll, func){
+  
+  if(Array.isArray(coll)){
+    
+    for(var i = 0; i < coll.length; i++){
+      func(coll[i],i)
+    }
+  }
+  else{
+    var acc = {};
+    for(var key in coll){
+      func(coll[key],key)
+    }
+  }
 
+}
+
+
+
+function filter(coll, predictor){
+  var acc = {};
+  if(Array.isArray(coll)){
+    acc = []
+  }
+  each(coll, function(element, i){
+    if(predictor(element,i)){
+      if(typeof i === 'number'){
+        acc.push(element)
+      }
+    else{
+      acc[i] = element
+    }
+  }
+})
+  return acc;
+}
+
+var staff = [
+  {name: {first: "Alyssa", last: "Hacker"}, birthDay: {month: "June", day: 5, year: 1987}},
+  {name: {first: "Ben", last: "Bitdiddle"}, birthDay: {month: "August", day: 19, year: 1984}},
+  {name: {first: "Eva", last: "Ator"}, birthDay: {month: "March", day: 29, year: 1980}},
+  {name: {first: "Lem", last: "Tweakit"}, birthDay: {month: "August", day: 11, year: 1989}},
+  {name: {first: "Louis", last: "Reasoner"}, birthDay: {month: "November", day: 17, year: 1992}}
+];
+
+
+function getPeopleBornIn(people, month){
+  return filter(people, function(person, i){
+      return person['birthDay']['month'] === month
+  })
+}
